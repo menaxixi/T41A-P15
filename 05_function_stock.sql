58f47e597e04f78a436f825b0635bf0a506de65a
menaxixi <179800@upslp.edu.mx>
2025-11-10 19:37:54 -0600

    Add stock_prod function to retrieve low stock items

--- 05_function_stock.sql
+++ 05_function_stock.sql
@@ -1,1 +1,6 @@
-
+CREATE OR REPLACE FUNCTION stock_prod(minimo INT)
+RETURNS TABLE(nom TEXT, stock INT) AS $$
+BEGIN   
+  RETURN QUERY SELECT name,cantidad FROM productos WHERE cantidad<minimo;
+END;
+$$ LANGUAGE plpgsql;
