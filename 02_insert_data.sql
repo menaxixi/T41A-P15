ac1cd17cd684de6f5afce734d8b883d1ca4b5c40
menaxixi <179800@upslp.edu.mx>
2025-11-10 19:36:52 -0600

    Insert initial data into productos, dep, and empleado

---
+++ 02_insert_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO productos (name,precio,cantidad) VALUES
+('Tablet',2500.00,20),
+('Camara',1250.00,5),
+('Computadora',12500.00,8),
+('Telefono', 9000.00,15),
+('USB',120.00,30),
+('Bocina',1740.00,20);
+
+
+INSERT INTO dep(nombre_dep) VALUES
+('ITI'),
+('Marketing'),
+('Recursos humanos');
+
+INSERT INTO empleado(nombre,id_dep) VALUES
+('Coral Jazmin', 1),
+('Bryan Yareth',1),
+('Mildred Sanchez',1),
+('Belen', 2),
+('Mario Lopez',2),
+('Juan Lorenzo',3);
